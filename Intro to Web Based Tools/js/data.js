ea2ffa1b7f09679985338190c0c1a30bb8a6049c
u0457617 <59180252+u0457617@users.noreply.github.com>
2020-05-27 19:34:09 -0600

    Add files via upload

---
+++ Intro to Web Based Tools/js/data.js
@@ -0,0 +1,19 @@
+var array_questions = [
+    {
+        statement: "Anakin was 9 when he was first found by the Jedi.",
+        sub_topic: "phantom menace",
+        answer_key: "option1",
+        hint: "https://www.youtube.com/watch?v=b8QGlwSt6vs"
+    },
+    {
+        statement: "Han Solo was frozen in liquid nitrogen.",
+        topic: "random star wars knowledge",
+        sub_topic: "Empire Strikes Back",
+        answer_key: "option2",
+        hint: "https://www.youtube.com/results?search_query=han+solo+frozen+in+carbonite+scene",
+    }
+    
+];
+
+var array_answers = [];
+
---
+++ Intro to Web Based Tools/js/variable.js
@@ -0,0 +1,10 @@
+var message = "Hello World";
+console.log(message);
+
+// Sections in the HTM: CSS Grid
+var sec_landing = document.getElementById("sec_landing"),
+    sec_quiz = document.activeElement("sec_quiz");
+
+
+//Containers to display feedback
+Var cont_success = document.getElementById("cont_success")
