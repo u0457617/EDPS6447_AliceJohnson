68dfbcd4b971fa56ccd5b6959ff56ff1ab7c0656
u0457617 <59180252+u0457617@users.noreply.github.com>
2020-06-03 10:21:49 -0600

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,38 @@
+var array_questions = [
+    {
+        statement: "Anakin was 9 when he was first found by the Jedi.",
+        sub_topic: "phantom menace",
+        answer_key: "option1",
+        hint: "https://www.youtube.com/watch?v=b8QGlwSt6vs",
+        type: "true_false",
+		labels: ["True", "False", "N/A", "N/A"],
+		video: { display: "false", source: "N/A" },
+		help: ["Sorry, I don't have any additional information to share for this question."],
+	},
+    {
+        statement: "Han Solo was frozen in liquid nitrogen.",
+        topic: "random star wars knowledge",
+        sub_topic: "Empire Strikes Back",
+        answer_key: "option2",
+        type: "true_false",
+        hint: "https://www.youtube.com/results?search_query=han+solo+frozen+in+carbonite+scene",	
+        labels: ["True", "False", "N/A", "N/A"],
+		video: { display: "false", source: "N/A" },
+		help: ["Sorry, I don't have any additional information to share for this question."],
+    },
+    {
+        statement: "What is Rey's parentage?",
+        topic: "random star wars knowledge",
+        sub_topic: "rise of skywalker",
+        answer_key: "option2",
+        type: "mulitple_choice",
+        hint: "https://www.youtube.com/results?search_query=han+solo+frozen+in+carbonite+scene",
+    	labels: ["Nobodies from nowhere", "Luke's love child", "Granddaughter of Darth Sidious", "Leah and Han's daughter"],
+		video: { display: "false", source: "N/A" },
+		help: ["Sorry, I don't have any additional information to share for this question."],
+    }
+    
+];
+
+var array_answers = [];
+
---
+++ js/listener.js
@@ -0,0 +1,20 @@
+btn_start.addEventListener("click", function(){
+    // Add block of code
+    console.log("Click event - btn.start");
+});
+
+btn_submit.addEventListener("click", function(){
+    console.log("Click event - btn_submit");
+});
+btn_previous.addEventListener("click", function(){
+    console.log("Click event - btn_submit");
+});
+btn_hint.addEventListener("click", function(){
+    console.log("Click event - btn_submit");
+});
+btn_next.addEventListener("click", function(){
+    console.log("Click event - btn_submit");
+});
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("DOM fully loaded and parsed");
+})
